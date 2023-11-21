7bced52b5ec17c19b258691ab7f8023d837da460
roti <robert.timm@wikimedia.de>
2023-11-21 08:02:58 +0000

    feat: extend eval logic

--- eval.py
+++ eval.py
@@ -1,92 +1,81 @@
-import askwikidata
+from askwikidata import AskWikidata
+from typing import Callable
 
-num_chunks = 5
 
+def kai_wegner_date_correct(text):
+    t = text.lower()
+    return "27" in t and "2023" in t and ("4" in t or "april" in t)
+    
 quiz = [
-    # { "q": "Who is the current mayor of Berlin?", "a": "Kai Wegner" },
+    {"q": "What is Berlin?", "a": ["capital", "germany", "city"]},
+    {"q": "Mayor of Berlin", "a": "Kai Wegner"},
+    {"q": "mayor berlin", "a": "Kai Wegner"},
+    {"q": "since when is kay wegner mayor of berlin?", "a": kai_wegner_date_correct},
+    # {"q": "Who is the current mayor of Berlin?", "a": "Kai Wegner"},
     # { "q": "What is the population of Berlin?", "a": "3755251" },
     # { "q": "Can you name a twin city of Berlin?", "a": [ "Los Angeles","Paris","Madrid","Istanbul","Warsaw","Moscow","City of Brussels","Budapest","Tashkent","Mexico City","Beijing","Jakarta","Tokyo","Buenos Aires","Prague","Windhoek","London","Sofia","Tehran","Seville","Copenhagen","Kyiv","Brasília","Santo Domingo","Algiers","Rio de Janeiro" ] },
-    # { "q": "Which River runs through Berlin?", "a": "Spree" },
-    # { "q": "Who is the current mayor of Paris?", "a": "Anne Hidalgo" },
-    # { "q": "What is the population of Paris?", "a": "2145906" },
+    # {"q": "Which River runs through Berlin?", "a": "Spree"},
+    # {"q": "Who is the current mayor of Paris?", "a": "Anne Hidalgo"},
+    # {"q": "What is the population of Paris?", "a": "2145906"},
     # { "q": "Can you name a twin city of Paris?", "a": [ "Rome","Tokyo","Kyoto","Berlin","Ramallah","Seoul","Cairo","Chicago","Torreón","San Francisco","Kyiv","Washington, D.C.","Marrakesh","Porto Alegre","Dubai","Beijing","Mexico City","Saint Petersburg" ] },
-    # { "q": "Which River runs through Paris?", "a": "Seine" },
-    # { "q": "Who is the current mayor of London?", "a": "Sadiq Khan" },
+    # {"q": "Which River runs through Paris?", "a": "Seine"},
+    # {"q": "Who is the current mayor of London?", "a": "Sadiq Khan"},
     # { "q": "What is the population of London?", "a": "8799728" },
     # { "q": "Can you name a twin city of London?", "a": [ "Berlin","Mumbai","New York City","Algiers","Sofia","Moscow","Tokyo","Beijing","Karachi","Zagreb","Tehran","Arequipa","Delhi","Bogotá","Johannesburg","Kuala Lumpur","Oslo","Sylhet","Shanghai","Baku","Buenos Aires","Istanbul","Los Angeles","Podgorica","New Delhi","Phnom Penh","Jakarta","Amsterdam","Bucharest","Santo Domingo","La Paz","Mexico City" ] },
-    # { "q": "Which River runs through London?", "a": "River Thames" },
-    { "q": "Who is the current mayor of Prague?", "a": "Bohuslav Svoboda" },
+    # {"q": "Which River runs through London?", "a": "River Thames"},
+    # {"q": "Who is the current mayor of Prague?", "a": "Bohuslav Svoboda"},
     # { "q": "What is the population of Prague?", "a": "1357326" },
     # { "q": "Can you name a twin city of Prague?", "a": [ "Berlin","Copenhagen","Miami-Dade County","Nuremberg","Luxembourg","Guangzhou","Hamburg","Helsinki","Nîmes","Prešov","Rosh HaAyin","Teramo","Bamberg","City of Brussels","Frankfurt","Jerusalem","Moscow","Saint Petersburg","Chicago","Taipei","Terni","Ferrara","Trento","Monza","Lecce","Naples","Vilnius","Istanbul","Sofia","Buenos Aires","Athens","Bratislava","Madrid","Tunis","Brussels metropolitan area","Amsterdam","Phoenix","Tirana","Kyoto","Cali","Drancy","Beijing","Shanghai","Tbilisi" ] },
-    # { "q": "Which River runs through Prague?", "a": "Vltava" },
+    # {"q": "Which River runs through Prague?", "a": "Vltava"},
 ]
 
-correct_contexts = 0
+askwikidata = AskWikidata(retrieval_chunks=3)
+askwikidata.setup()
 
-mistral_correct = 0
-llama_correct = 0
-openai_correct = 0
+correct_context = 0
+correct_answer = 0
 
-for i, qa in enumerate(quiz):
-    question=qa["q"]
-    answer=qa["a"]
-    embeddings_model = SentenceTransformer(embedding_model_name)
-    question_embedding = embeddings_model.encode(question)
-    question_embedding = [float(value) for value in question_embedding]
-    nearest_ids = index.get_nns_by_vector(
-        question_embedding, num_chunks, include_distances=True
-    )
-    context=""
-    for n in zip(nearest_ids[0], nearest_ids[1]):
-        chunk_id=n[0]
-        distance=n[1]
-        # print("\n")
-        # print(n)
-        # print(chunks[n[0]].page_content)
-        context+=chunks[chunk_id].page_content+"\n"
+for i, q in enumerate(quiz):
+    question = q["q"]
+    expected_answer = q["a"]
 
-    print("\n####################################\n")
-    print("Question: ", question)
-    print("Expected answer: ", answer)
-    if answer in context:
-        # print("context has answer")
-        correct_contexts+=1
+    if isinstance(expected_answer, str):
+        expected_answer = expected_answer.lower()
+    if isinstance(expected_answer, list):
+        expected_answer = [answer_part.lower() for answer_part in expected_answer]
+
+    # print(expected_answer)
+
+    print(question)
+    context = askwikidata.context(question)
+    answer = askwikidata.ask(question)
+    answer_lower = askwikidata.ask(question).lower()
+    context = context.lower()
+    # print(answer)
+
+    if (isinstance(expected_answer, str) and expected_answer in context) or (
+        isinstance(expected_answer, list)
+        and all((answer_part) in context for answer_part in expected_answer) or
+        isinstance(expected_answer, Callable) and expected_answer(context) == True
+    ):
+        correct_context += 1
+        print(f"correct context ({correct_context} of {len(quiz)})")
     else:
-        # print("context MISSING answer")
+        print("### WRONG context")
         print(context)
+        print("###")
 
-    # print("mistral: " + ask_mistral(question, context))
-    mistral_response = ask_mistral(question, context)
-    # print("llama: " + ask_llama_runpod(question, context))
-    llama_response = ask_llama_runpod(question, context)
-    # print("openai: " + ask_openai(question, context))
-    openai_response = ask_openai(question, context)
-
-    print(mistral_response)
-    if answer in mistral_response.replace(',', '').replace('.', ''):
-        mistral_correct+=1
-        print("mistral correct")
+    if (isinstance(expected_answer, str) and expected_answer in answer_lower) or (
+        isinstance(expected_answer, list)
+        and all((answer_part) in answer_lower for answer_part in expected_answer) or 
+        isinstance(expected_answer, Callable) and expected_answer(answer_lower) == True
+    ):
+        correct_answer += 1
+        print(f"correct answer '{answer}' ({correct_answer} of {len(quiz)})")
     else:
-        print("mistral wrong")
+        print(f"### wrong answer '{answer}'")
+        print("###")
 
-    print(llama_response)
-    if answer in llama_response.replace(',', '').replace('.', ''):
-        llama_correct+=1
-        print("llama correct")
-    else:
-        print("llama wrong")
-
-    print(openai_response)
-    if answer in openai_response.replace(',', '').replace('.', ''):
-        openai_correct+=1
-        print("openai correct")
-    else:
-        print("openai wrong")
-
-# print(f"Question: {question}")
-# print("mistral: " + ask_mistral(question, context))
-# print("llama: " + ask_llama_runpod(question, context))
-# print("openai: " + ask_openai(question, context))
-print("embedding_model\tchunk_size\tnum_chunks\tcorrect_contexts\tmistral_correct\tllama_correct\topenai_correct") 
-print(f"{embedding_model_name}\t{chunk_size}\t{num_chunks}\t{correct_contexts}\t{mistral_correct}\t{llama_correct}\t{openai_correct}")
- 
+print(f"\n# Results")
+print(f"Correct contexts: {correct_context} of {len(quiz)}")
+print(f"Correct answers: {correct_answer} of {len(quiz)}")
