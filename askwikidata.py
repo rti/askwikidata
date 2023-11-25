d06e1b030ea62ad869c5441e008dfd0af62dfb25
roti <robert.timm@wikimedia.de>
2023-11-25 20:20:29 +0000

    fix: use embed_query instead of embed_documents

--- askwikidata.py
+++ askwikidata.py
@@ -146,7 +146,9 @@ class AskWikidata:
 
     def retrieve(self, query: str) -> pd.DataFrame:
         print("Retrieving...")
-        query_embed = self.embedding_model.embed_documents([query])[0]
+        # TODO: what works better?
+        # query_embed = self.embedding_model.embed_documents([query])[0]
+        query_embed = self.embedding_model.embed_query(query)
         query_embed_float = [float(value) for value in query_embed]
         nns = self.index.get_nns_by_vector(
             query_embed_float, self.retrieval_chunks, include_distances=True
