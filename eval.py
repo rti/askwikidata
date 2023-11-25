12a61d632b1d5e3b9a5d40fbcc465fb97bf2ebc4
roti <robert.timm@wikimedia.de>
2023-11-25 11:09:21 +0000

    feat: eval results as json

--- eval.py
+++ eval.py
@@ -1,11 +1,31 @@
 from askwikidata import AskWikidata
-from typing import Callable
+from typing import Callable, List, Dict, Optional
+from dataclasses import dataclass, field, asdict
 from pprint import pprint
+import json
 
 
-def kai_wegner_date_correct(text):
+def kai_wegner_date_correct(text: str):
     t = text.lower()
     return "27" in t and "2023" in t and ("4" in t or "april" in t)
+
+
+def correct(answer: str, expected: str | list | Callable) -> bool:
+    answer_lower = answer.lower()
+    expected_lower = None
+    if isinstance(expected, str):
+        expected_lower = expected.lower()
+    if isinstance(expected, list):
+        expected_lower = [answer_part.lower() for answer_part in expected]
+
+    return (
+        (isinstance(expected_lower, str) and expected_lower in answer_lower)
+        or (
+            isinstance(expected_lower, list)
+            and all((answer_part) in answer_lower for answer_part in expected_lower)
+        )
+        or (isinstance(expected, Callable) and expected(answer_lower) == True)
+    )
 
 
 # fmt: off
@@ -32,153 +52,140 @@ quiz = [
     {"q": "Can you name all twin cities of Prague?", "a": [ "Berlin", "Copenhagen", "Miami-Dade County", "Nuremberg", "Luxembourg", "Guangzhou", "Hamburg", "Helsinki", "Nîmes", "Prešov", "Rosh HaAyin", "Teramo", "Bamberg", "City of Brussels", "Frankfurt", "Jerusalem", "Moscow", "Saint Petersburg", "Chicago", "Taipei", "Terni", "Ferrara", "Trento", "Monza", "Lecce", "Naples", "Vilnius", "Istanbul", "Sofia", "Buenos Aires", "Athens", "Bratislava", "Madrid", "Tunis", "Brussels metropolitan area", "Amsterdam", "Phoenix", "Tirana", "Kyoto", "Cali", "Drancy", "Beijing", "Shanghai", "Tbilisi", ], },
     {"q": "Can you name all twinned administrative bodies of Prague?", "a": [ "Berlin", "Copenhagen", "Miami-Dade County", "Nuremberg", "Luxembourg", "Guangzhou", "Hamburg", "Helsinki", "Nîmes", "Prešov", "Rosh HaAyin", "Teramo", "Bamberg", "City of Brussels", "Frankfurt", "Jerusalem", "Moscow", "Saint Petersburg", "Chicago", "Taipei", "Terni", "Ferrara", "Trento", "Monza", "Lecce", "Naples", "Vilnius", "Istanbul", "Sofia", "Buenos Aires", "Athens", "Bratislava", "Madrid", "Tunis", "Brussels metropolitan area", "Amsterdam", "Phoenix", "Tirana", "Kyoto", "Cali", "Drancy", "Beijing", "Shanghai", "Tbilisi", ], },
     {"q": "Which River runs through Prague?", "a": "Vltava"},
-    # {"q": "What is the capital of Hawaii?", "a": "Honolulu"},
+    {"q": "What is the capital of Hawaii?", "a": "Honolulu"},
+    {"q": "What is the elevation of Bobo-Dioulasso?", "a": "445"},
 ]
-# fmt: off
+# fmt: on
 
-hyperparameters_list = [ {
-    "chunk_size": 1280,
-    "chunk_overlap": 0,
-    "index_trees": 10,
-    "retrieval_chunks": 16,
-    "context_chunks": 5,
-    # "embedding_model_name": "BAAI/bge-small-en-v1.5",
-    "embedding_model_name": "BAAI/bge-base-en-v1.5",
-    # "embedding_model_name": "BAAI/bge-large-en-v1.5",
-    "reranker_model_name": "BAAI/bge-reranker-base",
-    # "qa_model_name": "mistral-7b-instruct-v0.1",
-    # "qa_model_url": "https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.1",
-    "qa_model_url": "https://api-inference.huggingface.co/models/meta-llama/Llama-2-7b-chat-hf",
-} ] 
+configurations = [
+    {
+        "chunk_size": 1280,
+        "chunk_overlap": 0,
+        "index_trees": 10,
+        "retrieval_chunks": 16,
+        "context_chunks": 5,
+        # "embedding_model_name": "BAAI/bge-small-en-v1.5",
+        "embedding_model_name": "BAAI/bge-base-en-v1.5",
+        # "embedding_model_name": "BAAI/bge-large-en-v1.5",
+        "reranker_model_name": "BAAI/bge-reranker-base",
+        # "reranker_model_name": "BAAI/bge-reranker-large",
+        # "qa_model_url": "https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.1",
+        "qa_model_url": "https://api-inference.huggingface.co/models/meta-llama/Llama-2-7b-chat-hf",
+    }
+]
 
-for hyperparameters_list in hyperparameters_list:
-    print("⚙ Hyperparams")
-    pprint(hyperparameters_list)
 
-    askwikidata = AskWikidata(**hyperparameters_list)
+@dataclass
+class QERCA:
+    question: str
+    expected_answer: str | List[str]
+    retrieved_context: str
+    reranked_context: Optional[str] = None
+    answer: Optional[str] = None
+
+
+@dataclass
+class EvalResult:
+    config: Dict[str, str | int]
+    total_questions: int
+    correct_retrievals: int = 0
+    correct_reranks: int = 0
+    correct_answers: int = 0
+    correct_answers_plain: int = 0
+    failed_retrieval_questions: List[QERCA] = field(default_factory=list)
+    failed_rerank_questions: List[QERCA] = field(default_factory=list)
+    failed_answer_questions: List[QERCA] = field(default_factory=list)
+
+
+eval_results: List[EvalResult] = []
+
+for config in configurations:
+    pprint(config)
+
+    eval_result = EvalResult(config, len(quiz))
+    eval_results.append(eval_result)
+
+    askwikidata = AskWikidata(**config)
     askwikidata.setup()
     # askwikidata.print_data()
-
-    correct_retrieved_contexts = 0
-    correct_reranked_contexts = 0
-    correct_answers = 0
-    failed_retrieve = []
-    failed_rerank = []
-    failed_answer = []
 
     for i, q in enumerate(quiz):
         question = q["q"]
         expected_answer = q["a"]
 
-        if isinstance(expected_answer, str):
-            expected_answer = expected_answer.lower()
-        if isinstance(expected_answer, list):
-            expected_answer = [answer_part.lower() for answer_part in expected_answer]
-
         print("")
         print("Question:", question)
         print("Expected answer:", expected_answer)
+
+
+        answer_plain = askwikidata.llm_generate_plain(question)
+        if correct(answer_plain, expected_answer):
+            eval_result.correct_answers_plain += 1
+            print("🙈 Plain Answer correct:", answer_plain)
+        else:
+            print("👍 WRONG Plain Answer:", answer_plain)
+
 
         retrieved = askwikidata.retrieve(question)
         retrieved_context = askwikidata.context(retrieved)
         retrieved_context_lower = retrieved_context.lower()
 
-        if (
-            (
-                isinstance(expected_answer, str)
-                and expected_answer in retrieved_context_lower
-            )
-            or (
-                isinstance(expected_answer, list)
-                and all(
-                    (answer_part) in retrieved_context_lower
-                    for answer_part in expected_answer
-                )
-            )
-            or (
-                isinstance(expected_answer, Callable)
-                and expected_answer(retrieved_context_lower) == True
-            )
-        ):
-            correct_retrieved_contexts += 1
+        if correct(retrieved_context, expected_answer):
+            eval_result.correct_retrievals += 1
             print("✅ Retrieved Context")
         else:
             print("‼️ WRONG Retrieved Context")
-            failed_retrieve.append(question)
-            # print(retrieved_context)
+            eval_result.failed_retrieval_questions.append(
+                QERCA(question, expected_answer, retrieved_context)
+            )
             continue
 
         reranked, rerank_time = askwikidata.rerank(question, retrieved)
         print(f"  {int(rerank_time)} seconds.")
         reranked_context = askwikidata.context(reranked)
-        reranked_context_lower = reranked_context.lower()
 
-        if (
-            (
-                isinstance(expected_answer, str)
-                and expected_answer in reranked_context_lower
-            )
-            or (
-                isinstance(expected_answer, list)
-                and all(
-                    (answer_part) in reranked_context_lower
-                    for answer_part in expected_answer
-                )
-            )
-            or (
-                isinstance(expected_answer, Callable)
-                and expected_answer(reranked_context_lower) == True
-            )
-        ):
-            correct_reranked_contexts += 1
+        if correct(reranked_context, expected_answer):
+            eval_result.correct_reranks += 1
             print("✅ Reranked Context")
         else:
             print("‼️ WRONG Reranked Context")
-            failed_rerank.append(question)
-            # print(retrieved_context)
+            eval_result.failed_rerank_questions.append(
+                QERCA(question, expected_answer, retrieved_context, reranked_context)
+            )
             continue
 
         answer = askwikidata.llm_generate(question, reranked)
-        answer_lower = answer.lower()
 
-        if (
-            (
-                isinstance(expected_answer, str)
-                and expected_answer in answer_lower
-            )
-            or (
-                isinstance(expected_answer, list)
-                and all(
-                    (answer_part) in answer_lower
-                    for answer_part in expected_answer
-                )
-            )
-            or (
-                isinstance(expected_answer, Callable)
-                and expected_answer(answer_lower) == True
-            )
-        ):
-            correct_answers += 1
+        if correct(answer, expected_answer):
+            eval_result.correct_answers += 1
             print("✅ Answer:", answer)
         else:
             print("‼️ WRONG Answer:", answer)
-            failed_answer.append(question)
-            # print(retrieved_context)
-            continue
+            eval_result.failed_answer_questions
+            eval_result.failed_retrieval_questions.append(
+                QERCA(
+                    question,
+                    expected_answer,
+                    retrieved_context,
+                    reranked_context,
+                    answer,
+                )
+            )
 
 
+for eval_result in eval_results:
     print("")
-    print("*************\n"
-          "🔍 Results 🔎\n")
+    print("***************************************")
+    print("          🔍 Results 🔎\n")
+    print("\n")
+    pprint(eval_result.config)
+    print("\n")
+    pprint(eval_result, width=120, depth=1)
+    print("\n")
+    print("***************************************")
+    print("\n")
 
-    pprint(hyperparameters_list)
-    print(f"Retrieved Contexts: {correct_retrieved_contexts} of {len(quiz)}")
-    print(f"Reranked Contexts: {correct_reranked_contexts} of {len(quiz)}")
-    print(f"Correct Answers: {correct_answers} of {len(quiz)}")
-    if len(failed_retrieve): print(f"Failed Retrieve for questions:") 
-    for q in failed_retrieve: print(" ", q)
-    if len(failed_rerank): print(f"Failed Rerank for questions:") 
-    for q in failed_rerank: print(" ", q)
-    if len(failed_answer): print(f"Failed Answer for questions:") 
-    for q in failed_answer: print(" ", q)
-    print("*************\n")
+
+with open(f"eval_resuls.json", "w") as file:
+    for eval_result in eval_results:
+        file.write(json.dumps(asdict(eval_result)))
