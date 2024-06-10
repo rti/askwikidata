6917efcac5687d6e3621e14526d0d6af0a87291d
Robert Timm <mail@rtti.de>
2024-06-10 18:08:14 +0000

    feat: modularize

--- read_wikidata_dump.py
+++ read_wikidata_dump.py
@@ -6,6 +6,66 @@ import asyncio
 import aiofiles
 
 
+async def process(pool, line_handler_func, chunk_of_lines):
+    return pool.map(line_handler_func, chunk_of_lines)
+
+
+async def readlines(file, chunk_size):
+    return await file.readlines(chunk_size)
+
+
+async def process_file(file_path, line_handler_func, num_processes, chunk_size):
+    with Pool(num_processes) as pool:
+        start = time.time()
+        line_per_ms_values = []
+        iterations = 0
+
+        file = await aiofiles.open(file_path, mode="r")
+
+        read_task = asyncio.create_task(readlines(file, chunk_size))
+
+        while True:
+            chunk_of_lines = await read_task
+            if not chunk_of_lines:
+                break
+
+            read_task = asyncio.create_task(readlines(file, chunk_size))
+            process_task = asyncio.create_task(
+                process(pool, line_handler_func, chunk_of_lines)
+            )
+            results = await process_task
+
+            for result in results:
+                if result is not None:
+                    print(result)
+
+            time_per_iteration_ms = (time.time() - start) * 1000
+            lines_per_ms = len(chunk_of_lines) / time_per_iteration_ms
+            line_per_ms_values.append(lines_per_ms)
+            line_per_ms_values = line_per_ms_values[-5:]
+            lines_per_ms_avg = sum(line_per_ms_values) / len(line_per_ms_values)
+            iterations += 1
+            print(
+                f"{iterations:5}: {lines_per_ms:.2f} (avg {lines_per_ms_avg:.2f}) entities per ms",
+                file=sys.stderr,
+            )
+
+            # if iterations == 4:
+            #     break
+
+            start = time.time()
+
+        await file.close()
+
+
+def read_wikidata_dump(dump_file, line_handler_func):
+    threads = int(cpu_count() * 2)
+    chunksize = int(1024 * 1024 * 1024 * 1)
+    print(f"using {threads} processes, {chunksize} as chunk_size", file=sys.stderr)
+    asyncio.run(process_file(dump_file, line_handler_func, threads, chunksize))
+
+
+# Example usage
 def process_line(line):
     # print(f"'{line}'", file=sys.stderr)
 
@@ -35,64 +95,7 @@ def process_line(line):
         return json.dumps(entity)
 
 
-async def process(pool, chunk_of_lines):
-    return pool.map(process_line, chunk_of_lines)
-
-
-async def readlines(file, chunk_size):
-    return await file.readlines(chunk_size)
-
-
-async def process_file(file_path, num_processes, chunk_size):
-    with Pool(num_processes) as pool:
-        start = time.time()
-        line_per_ms_values = []
-        iterations = 0
-
-        file = await aiofiles.open(file_path, mode="r")
-
-        read_task = asyncio.create_task(readlines(file, chunk_size))
-
-        while True:
-            chunk_of_lines = await read_task
-            if not chunk_of_lines:
-                break
-
-            read_task = asyncio.create_task(readlines(file, chunk_size))
-            process_task = asyncio.create_task(process(pool, chunk_of_lines))
-            results = await process_task
-
-            for result in results:
-                if result is not None:
-                    print(result)
-
-            time_per_iteration_ms = (time.time() - start) * 1000
-            lines_per_ms = len(chunk_of_lines) / time_per_iteration_ms
-            line_per_ms_values.append(lines_per_ms)
-            line_per_ms_values = line_per_ms_values[-5:]
-            lines_per_ms_avg = sum(line_per_ms_values) / len(line_per_ms_values)
-            iterations += 1
-            print(
-                f"{iterations:5}: {lines_per_ms:.2f} (avg {lines_per_ms_avg:.2f}) entities per ms",
-                file=sys.stderr,
-            )
-
-            # if iterations == 4:
-            #     break
-
-            start = time.time()
-
-        await file.close()
-
-
 if __name__ == "__main__":
-    threads = int(cpu_count() * 2)
-    chunksize = int(1024 * 1024 * 1024 * 1)
-    print(f"using {threads} processes, {chunksize} as chunk_size", file=sys.stderr)
-    asyncio.run(
-        process_file(
-            "/home/rti/tmp/wikidata-20240514/wikidata-20240514.json",
-            threads,
-            chunksize,
-        )
+    read_wikidata_dump(
+        "/home/rti/tmp/wikidata-20240514/wikidata-20240514.json", process_line
     )
